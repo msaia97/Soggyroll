6308f19f709356a9df0fb742634571bf1be22e7f
msaia97 <masonsaia@gmail.com>
2021-06-01 16:55:48 -0700

    Finished user controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UserController < ApplicationController
+    before_action :require_login, only: [:update, :destroy, :show]
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            render 'api/users/show'
+        else 
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
