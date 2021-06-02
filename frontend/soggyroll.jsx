bb8c4176af0a45692d51122ae6dbd2ae451ebf53
msaia97 <masonsaia@gmail.com>
2021-06-01 23:39:13 -0700

    Made session thunk action creators

--- frontend/soggyroll.jsx
+++ frontend/soggyroll.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //components
 import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    // const store = configureStore();
+    const store = configureStore();
     ReactDOM.render(<Root />, root)
 });
