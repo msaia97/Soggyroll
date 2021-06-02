2b60b0638e0315ae8f499ee522f5de5c39657a2e
msaia97 <masonsaia@gmail.com>
2021-06-02 16:19:26 -0700

    Made session containers and form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Redirect, Switch, Link, Hashrouter } from 'react-router-dom';
 
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -35,6 +35,7 @@ class SessionForm extends React.Component{
     }
 
     render(){
+        debugger;
         return(
             <div className="login-form-container">
                 <form onSubmit={this.handleSubmit} className="login-form-box">
