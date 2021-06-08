f7ce4913f2d65c9f17510ddd6ad743fe86a6d64c
msaia97 <masonsaia@gmail.com>
2021-06-08 11:55:18 -0700

    Got shos pics to render on the page

--- app/views/api/animes/index.json.jbuilder
+++ app/views/api/animes/index.json.jbuilder
@@ -1,6 +1,6 @@
 @animes.each do |anime|
     json.set! anime.id do 
         json.partial! 'anime',  anime: anime
+        json.cover_photo url_for(anime.cover_photo) if anime.cover_photo.attached?
     end
-    json.photoUrl url_for(anime.cover_photo)
 end 
--- frontend/components/animes/animes.jsx
+++ frontend/components/animes/animes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { receiveShow } from '../../actions/show_actions';
 
 class Animes extends React.Component {
     constructor(props){
@@ -12,33 +11,17 @@ class Animes extends React.Component {
             cover_photo: null
         };
     }
-
-
+    
     componentDidMount(){
-        this.props.getAnime(this.props.anime.id);
-    //    targetAnime.append(cover_photo);
     }
-
 
     render(){
         return(
-            // <div className="anime-template" >
-            //     <br />
-            //     <li>{this.props.anime.title}</li>
-            //     <li>{this.props.anime.description}</li>
-            //     <br />
-            // </div>
-
-            // could possibly look like this
             <div className="anime-template">
-                <div className="anime-img">
-                    {this.props.anime.cover_photo}
-                </div>
-                <div className="anime-title">
-                      <Link to="/animes/{this.props.anime.id}" className="show-title-link"> 
-                         <b>{this.props.anime.title}</b>
-                      </Link>
-                </div>
+                <Link to="/animes/{this.props.anime.id}" className="show-title-link">
+                    <img className="anime-img" src={this.props.anime.cover_photo} />   
+                    <b>{this.props.anime.title}</b>
+                </Link>
             </div>
         )
     }
