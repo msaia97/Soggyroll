b4c493f48126e9311254a2ebb5024aaccc1442ad
msaia97 <masonsaia@gmail.com>
2021-08-04 13:57:43 -0700

    Got the videos the right size

--- frontend/components/episodes/episode_show.jsx
+++ frontend/components/episodes/episode_show.jsx
@@ -6,48 +6,17 @@ class EpisodeShow extends React.Component {
     }
 
     componentDidMount(){
-        // debugger
-        // this.props.getEpisode(this.props.match.params)
-        // console.log(this.state)
+       
     }
 
     render(){
         let episodes = this.props.episodes;
         let anime = this.props.anime;
         
-        // episodes.forEach(episode => {
-            //     if(episode.videoId === anime.id){
-                //         return(
-                    //             console.log(maybe)
-                    //             //  <div className="episode-show">
-                    //             //     <p>Episode show page!!!</p>
-                    //             //     <video src="episode.video"></video>
-                    //             // </div>
-                    //         )
-                    //     }else{
-                        //         return(
-                            //             null
-                            //         )
-                            //     }
-                            // })
-                            console.log(episodes[0])
         return(
-            <video  autoPlay  src={episodes[0].video} controls></video>
-            // <div>
-            //     {episodes.forEach(episode => {
-            //         if(episode.videoId === anime.id){
-            //             return(
-            //                 <div>
-            //                     <p>Working?</p>
-            //                  </div>
-            //             )
-            //         }else{
-            //             return(
-            //                 <p>Not working</p>
-            //             )
-            //         }
-            //     })}
-            // </div>
+            <div className="video-wrapper">
+                <video className="episode-vid" autoPlay  src={episodes[0].video} controls  ></video>
+            </div>
         )
     }
 }
