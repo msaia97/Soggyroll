de40a287abab675636b6e740eae734047af6c9b6
msaia97 <masonsaia@gmail.com>
2021-08-06 13:47:44 -0700

    Got some of actions

--- frontend/actions/queue_actions.js
+++ frontend/actions/queue_actions.js
@@ -1,3 +1,6 @@
 import * as QueuedAPIUtil from '../util/queue_api_util';
 
-export const RECEIVE_Q
+export const RECEIVE_QUEUES = "RECEIVE_QUEUES";
+export const RECEIVE_QUEUED_ANIME = "RECEIVE_QUEUED_ANIME";
+export const RECEIVE_QUEUED_EPISODE = "RECEIVE_QUEUED_EPISODE";
+export const REMOVE_QUEUE = "REMOVE_QUEUE";
