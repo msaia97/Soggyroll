293a2e22563867463cae3ae64eed15ea7eb14df8
msaia97 <masonsaia@gmail.com>
2021-08-09 21:27:20 -0700

    Finished reducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux';
 import usersReducer from './users_reducer';
 import animesReducer from './animes_reducer';
 import episodesReducer from './episodes_reducer';
+import queuesReducer from "./queues_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     animes: animesReducer,
-    episodes: episodesReducer
+    episodes: episodesReducer,
+    queues: queuesReducer
 });
 
 export default entitiesReducer;
