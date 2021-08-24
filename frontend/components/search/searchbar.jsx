7b254c8f51d1fda1bd4ceedd03df65768f4b81b5
msaia97 <masonsaia@gmail.com>
2021-08-23 17:04:10 -0700

    Working more on search

--- frontend/components/search/searchbar.jsx
+++ frontend/components/search/searchbar.jsx
@@ -8,6 +8,7 @@ class SearchBar extends React.Component {
         this.state = {
             serach: "",
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     update(field) {
@@ -16,4 +17,32 @@ class SearchBar extends React.Component {
                 [field]: e.currentTarget.value,
             });
     }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const search = this.state.search;
+        this.setState({ search: "" });
+        this.props.histoty.push(`/seach/query="${search}`)
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit} className="search-section" >
+                    <input 
+                        type="text"
+                        placeholder="Search animes"
+                        value={this.state.search} 
+                        onChange={this.update("search")}
+                        className="search-bar"
+                    />
+                    <button type="submit" className="search-button">
+                        <i className="fa fa-search"></i>
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default withRouter(SearchBar);
